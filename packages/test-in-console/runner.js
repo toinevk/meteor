b94666860554417e674bd18f3070e377a80c7475
Toine van Kampen <toine.vankampen@gmail.com>
2018-04-15 14:24:53 +1000

    Test Test in Console for TravisCI
    Pass 1

--- packages/test-in-console/runner.js
+++ packages/test-in-console/runner.js
@@ -1,37 +1,42 @@
-var createPage = require("webpage").create;
-var system = require("system");
-var platform = system.args[1] || "local";
+var createPage = require('webpage').create;
+var system = require('system');
+var puppeteer = require('puppeteer');
+var platform = system.args[1] || 'local';
 var platformUrl = system.env.URL + platform;
 var testUrls = [
   // Additional application URLs can be added here to re-run tests in
   // PhantomJS with different query parameter-based configurations.
-  platformUrl,
+  platformUrl
 ];
 
-function runNextUrl() {
+async function runNextUrl() {
   var url = testUrls.shift();
-  if (! url) {
-    phantom.exit(0);
+  if (!url) {
+    await page.close();
     return;
   }
 
-  console.log("Running Meteor tests in PhantomJS... " + url);
+  console.log('Running Meteor tests in Puppeteer ' + url);
 
-  var page = createPage();
+  // --no-sandbox and --disable-setuid-sandbox required for CI compability
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+  console.log(await browser.version());
+  const page = await browser.newPage();
 
-  page.onConsoleMessage = function (message) {
-    console.log(message);
-  };
+  page.on('console', msg => {
+    console.log(msg);
+  });
 
-  page.open(url);
+  await page.goto(url);
 
-  function poll() {
+  async function poll() {
     if (isDone(page)) {
       var failCount = getFailCount(page);
       if (failCount > 0) {
-        phantom.exit(1);
+        await page.close();
+        console.log('Complete with errors');
       } else {
-        page.close();
+        await page.close();
         setTimeout(runNextUrl, 1000);
       }
     } else {
@@ -43,22 +48,22 @@ function runNextUrl() {
 }
 
 function isDone(page) {
-  return page.evaluate(function () {
-    if (typeof TEST_STATUS !== "undefined") {
+  return page.evaluate(function() {
+    if (typeof TEST_STATUS !== 'undefined') {
       return TEST_STATUS.DONE;
     }
 
-    return typeof DONE !== "undefined" && DONE;
+    return typeof DONE !== 'undefined' && DONE;
   });
 }
 
 function getFailCount(page) {
-  return page.evaluate(function () {
-    if (typeof TEST_STATUS !== "undefined") {
+  return page.evaluate(function() {
+    if (typeof TEST_STATUS !== 'undefined') {
       return TEST_STATUS.FAILURES;
     }
 
-    if (typeof FAILURES === "undefined") {
+    if (typeof FAILURES === 'undefined') {
       return 1;
     }
 
