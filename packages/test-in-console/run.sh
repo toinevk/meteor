c1c4d320ead074ff5b690cba8a48b29e017350e6
Toine van Kampen <toine.vankampen@gmail.com>
2018-04-15 19:53:17 +1000

    again
    .

--- packages/test-in-console/run.sh
+++ packages/test-in-console/run.sh
@@ -17,7 +17,7 @@ exec 3< <(meteor test-packages --driver-package test-in-console -p 4096 --exclud
 EXEC_PID=$!
 
 sed '/test-in-console listening$/q' <&3
-dev_bundle/lib/node_modules/puppeteer/index "$METEOR_HOME/packages/test-in-console/runner.js"
+dev_bundle/lib/node_modules/puppeteer/index.js "$METEOR_HOME/packages/test-in-console/runner.js"
 STATUS=$?
 
 pkill -TERM -P $EXEC_PID
