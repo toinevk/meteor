f18ccd496f4b9c312e55378a1b527e9701ce6832
Toine van Kampen <toine.vankampen@gmail.com>
2018-04-15 15:31:32 +1000

    Different tac
    test

--- packages/test-in-console/run.sh
+++ packages/test-in-console/run.sh
@@ -5,7 +5,7 @@ cd ../..
 export METEOR_HOME=`pwd`
 
 # Just in case these packages haven't been installed elsewhere.
-./meteor npm install -g puppeteer browserstack-webdriver
+./meteor npm install -g puppeteer phantomjs-prebuilt browserstack-webdriver
 
 export PATH=$METEOR_HOME:$PATH
 # synchronously get the dev bundle and NPM modules if they're not there.
@@ -17,7 +17,7 @@ exec 3< <(meteor test-packages --driver-package test-in-console -p 4096 --exclud
 EXEC_PID=$!
 
 sed '/test-in-console listening$/q' <&3
-exec 3< "$METEOR_HOME/packages/test-in-console/runner.js"
+./dev_bundle/bin/phantomjs "$METEOR_HOME/packages/test-in-console/runner.js"
 STATUS=$?
 
 pkill -TERM -P $EXEC_PID
